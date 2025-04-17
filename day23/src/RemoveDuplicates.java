efed06382d3896732270d6aa53486ae639d5110d
Ankita2k25 <ankita.mitra@nitsolution.in>
2025-04-17 16:10:22 +0530

    17thApril

--- day23/src/RemoveDuplicates.java
+++ day23/src/RemoveDuplicates.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
+//Remove duplicates from a list of integers.
 public class RemoveDuplicates {
     public static void main(String[] args) {
         List<Integer>list= Arrays.asList(2,5,9,1,3,2,5);
